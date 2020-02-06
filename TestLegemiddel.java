a38d155c367ae90d5f528e549611e5f2306a0c1d
Sebbafresh <Ereon@live.com>
2020-02-06 19:06:20 +0100

    Testene av legemidler er ferdig

--- TestLegemiddel.java
+++ TestLegemiddel.java
@@ -8,16 +8,41 @@ public class TestLegemiddel {
         //skal ikke fungere:
         //Legemiddel leg = new Legemiddel("feil", 150, 2);
 
-        System.out.println(van.hentNavn());
-        System.out.println(nark.hentID());
 
         //lag tester for alle de forskjellige objektene. Test alle metoder.
-        //bruk true false for å sjekke
 
-        boolean idTest = van.hentID() == nark.hentID();
+        //Tester Vanlig først
+        System.out.println("\nInformasjon om vanlig legemiddel: ");
+        System.out.println("--------------");
         System.out.println(van.hentID());
+        System.out.println(van.hentNavn());
+        System.out.println(van.hentPris());
+        System.out.println(van.hentVirkestoff());
+        van.settNyPris(1);
+        System.out.println("Ny pris: " + van.hentPris());
+
+
+        //tester Vanedannende deretter
+        System.out.println("\nInformasjon om vanedannende legemiddel: ");
+        System.out.println("--------------");
         System.out.println(vaned.hentID());
+        System.out.println(vaned.hentNavn());
+        System.out.println(vaned.hentPris());
+        System.out.println(vaned.hentVirkestoff());
+        vaned.settNyPris(500);
+        System.out.println("Ny pris: " + vaned.hentPris());
+
+
+        //tester narkotiske legemidler
+        System.out.println("\nInformasjon om narkotiske legemiddel: ");
+        System.out.println("--------------");
         System.out.println(nark.hentID());
+        System.out.println(nark.hentNavn());
+        System.out.println(nark.hentPris());
+        System.out.println(nark.hentVirkestoff());
+        nark.settNyPris(666);
+        System.out.println("Ny pris: " + nark.hentPris());
+
 
 
     }
